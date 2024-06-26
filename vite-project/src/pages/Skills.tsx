73d8970affd4b02cfda969ec5a758a8e4dfb6235
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-26 12:26:14 +0200

    Updated Skills.tsx

--- vite-project/src/pages/Skills.tsx
+++ vite-project/src/pages/Skills.tsx
@@ -1,25 +1,23 @@
-import "animate.css";
-
 export default function Skills() {
     const listSkills = [
         {
             name: "Symfony",
-            image: "https://api.iconify.design/logo:react.svg?color=%23ffffff",
+            image: "https://api.iconify.design/logos:symfony.svg?color=%23ffffff",
             desc: "Framework PHP for modern and robust web applications",
         },
         {
             name: "Adonis JS",
-            image: "https://api.iconify.design/logo:react.svg?color=%23ffffff",
+            image: "https://api.iconify.design/logos:adonisjs.svg?color=%23ffffff",
             desc: "Node FullStack Framework for modern and robust web applications",
         },
         {
             name: "React JS",
-            image: "https://api.iconify.design/logo:react.svg?color=%23ffffff",
+            image: "https://api.iconify.design/logos:react.svg?color=%23ffffff",
             desc: "JS Library for building UI web applications",
         },
         {
             name: "DevOps",
-            image: "https://api.iconify.design/logo:react.svg?color=%23ffffff",
+            image: "https://api.iconify.design/logos:git-icon.svg?color=%23ffffff",
             desc: "CI/CD with GitHub Action, VPS mounting and administration",
         },
     ];

c71ab865eb3a6dfbaf9a499e8ec6e9094ad669d6
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-26 11:22:40 +0200

    Updated Skills.tsx

--- vite-project/src/pages/Skills.tsx
+++ vite-project/src/pages/Skills.tsx
@@ -6,6 +6,14 @@ export default function Skills() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dignissimos sed sint incidunt accusamus officiis repellat dolorum 
                 numquam voluptate. Reprehenderit, sapiente laboriosam tenetur eligendi omnis nulla mollitia voluptatem sint dolorem?</p>
             </div>
+            <div className="">
+                <div className="">
+                    <img src="" alt="" />
+                    <h3 className="">Titre</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dignissimos sed sint incidunt accusamus officiis repellat
+                    dolorum numquam voluptate. Reprehenderit, sapiente laboriosam tenetur eligendi omnis nulla mollitia voluptatem sint dolorem?</p>
+                </div>
+            </div>
         </>
     )
 }

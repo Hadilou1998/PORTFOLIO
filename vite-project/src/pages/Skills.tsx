d5bd8de2f776a6e083fee88d19e4263fff6e2fe4
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-26 16:59:59 +0200

    Updated Skills.tsx and Home.tsx

--- vite-project/src/pages/Skills.tsx
+++ vite-project/src/pages/Skills.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import Project from "../components/Project";
+import Nav from "../components/Nav";
 
+/**
+ * Skills:
+ * Composant pour afficher les compétences,
+ * Il s'agit du contenu d'une page complète
+ * située sur la route : "/skills".
+ */
 export default function Skills() {
+    // On déclare une constant pour le choix du type de projet
+    const [choice, setChoice] = useState("All");
     
-    const [choice, setChoice] = useState("Symfony");
     
     const listSkills = [
         {
@@ -13,7 +21,7 @@ export default function Skills() {
         },
         {
             name: "Adonis JS",
-            image: "https://api.iconify.design/logos:adonisjs.svg?color=%23ffffff",
+            image: "https://api.iconify.design/logos:adonisjs-icon.svg?color=%23ffffff",
             desc: "Node FullStack Framework for modern and robust web applications",
         },
         {
@@ -27,34 +35,37 @@ export default function Skills() {
             desc: "CI/CD with GitHub Action, VPS mounting and administration",
         },
     ];
-    
     return (
-        <>
+        <main>
+            <Nav />
             <section className="flex gap-5 px-5 py-10">
                 <div className="w-1/3">
-                    <h1 className="mb-5 font-bold text-white text-3x1">Skillset</h1>
-                    <p className="leading-relaxed text-slate-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dignissimos sed sint incidunt accusamus officiis repellat dolorum
-                        numquam voluptate. Reprehenderit, sapiente laboriosam tenetur eligendi omnis nulla mollitia voluptatem sint dolorem?</p>
+                    <h1 className="mb-5 text-3xl font-bold text-white">Skillset</h1>
+                    <p className="leading-relaxed text-slate-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non libero, sapiente ipsum accusantium eligendi consectetur repellat nobis nisi optio rem itaque, hic atque doloribus saepe laboriosam corrupti omnis tempore possimus recusandae? Molestias error tempore quo! Adipisci temporibus, voluptates neque officiis quam voluptatum consectetur ad numquam doloribus accusamus, alias dolore ducimus.</p>
                 </div>
                 <ul className="grid w-2/3 grid-cols-2">
-                    {listSkills.map((skill) => (
-                        <li 
-                        key={skill.name}
-                        data-choice={skill.name}
-                        onClick={() => setChoice(skill.name)}
-                        >
-                            <img
-                                className="transition ease-in-out hover:scale-125"
-                                src={skill.image} alt={skill.name + " logo "} width={32} />
-                            <h3 className="text-lg font-semibold text-white">{skill.name}</h3>
-                            <p className="text-slate-600">{skill.desc}</p>
-                        </li>
-                    ))}
+                    {
+                        listSkills.map((skill) => (
+                            <li 
+                            className="cursor-pointer"
+                            key={ skill.name}
+                            data-choice={ skill.name }
+                            onClick={() => setChoice(skill.name)}
+                            >
+                                <img 
+                                    className="transition ease-in-out hover:scale-125"
+                                    src={ skill.image } alt={ skill.name + " logo" } width={ 32 } 
+                                />
+                                <h3 className="text-lg font-semibold text-white">{ skill.name }</h3>
+                                <p className="text-slate-600">{ skill.desc }</p>
+                            </li>
+                        ))
+                    }
                 </ul>
             </section>
             <section>
-                    <Project prop={choice}/>
+                <Project prop={choice}/>
             </section>
-        </>
+        </main>
     )
 }

48b0f3a6e67747bcb2cbc245535992448cb37173
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-28 09:44:42 +0200

    Updated Contact.tsx

--- vite-project/src/pages/Contact.tsx
+++ vite-project/src/pages/Contact.tsx
@@ -1,1 +1,10 @@
-export default function() {}
+import Nav from "../components/Nav";
+
+export default function Contact() {
+    return (
+        <main>
+            <Nav />
+            <h1>Contact</h1>
+        </main>
+    )
+}
